6836d9f10a65c5a933286e51dcf1517d510cb5e4
SAHIBADAS <diego.martinez.alberquilla@gmail.com>
2024-04-26 19:28:07 +0200

    Añadido codigo a clases Expense,ExpenseManager y Main

--- src/Expense.java
+++ src/Expense.java
@@ -1,4 +1,30 @@
 // This class represents an individual expense with two attributes: the amount spent and the expense category.
 public class Expense {
+    //variables miembro
+    private static double gasto;
+    private static String tipogasto;
+
+    //constructores 
+    public Expense(double gasto, String tipogasto) throws Invalid1, Invalid2{
+        this.gasto=gasto;
+        this.tipogasto=tipogasto;
+    }
+    
+    //metodos publicos
+	public static double getGasto() {
+		return gasto;
+	}
+
+	public static void setGasto(double g) {
+		gasto = g;
+	}
+
+	public static String getTipogasto() {
+		return tipogasto;
+	}
+
+	public static void setTipogasto(String tipogasto) {
+		tipogasto = tipogasto;
+	}
     
 }
--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -1,5 +1,18 @@
 // This class manages a list of Expense objects and includes methods to add,
 // list, and summarize expenses, as well as to validate inputs and handle file data.
-public class ExpenseManager {
-    
-}
+public class ExpenseManager{
+	
+	//inicializo 2 arrays para guardar gasto(int) y su tipo (String)
+	static int indicegasto = 0;
+    public static double[] gastod = new double[100];
+    public static String[] gastotipo = new String[100];
+ 
+	
+
+    //método para añadir gasto
+    public static void addGasto(Expense g) {
+    	   gastod[indicegasto]= g.getGasto();
+    	   gastotipo[indicegasto]=g.getTipogasto();
+    	   indicegasto++;
+       }
+    }
