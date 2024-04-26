0b90fa9675a23634fe2bed4b9c682e1b333f55d7
VO2003 <olmedoaznarveronica@gmail.com>
2024-04-26 20:07:23 +0200

    add: Methods of Part 2

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -2,4 +2,11 @@
 // list, and summarize expenses, as well as to validate inputs and handle file data.
 public class ExpenseManager {
     
+    static void saveDataFromFile(){
+        //TODO: add logic to upload file
+    }
+
+    static void loadData(){
+        //TODO: add logic to load data
+    }
 }
