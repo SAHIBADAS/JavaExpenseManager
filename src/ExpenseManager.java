692aa9eb0dfbe258ee5d05f67759b7e7fc029739
34644 <34644@SantiPC>
2024-04-26 20:53:48 +0200

    Feature-list-summarize-expenses

--- src/ExpenseManager.java
+++ src/ExpenseManager.java
@@ -1,5 +1,15 @@
 // This class manages a list of Expense objects and includes methods to add,
 // list, and summarize expenses, as well as to validate inputs and handle file data.
 public class ExpenseManager {
+	public static void sumarizarGastos(String tipo) {
+    	double sum = 0;
+    	for(int i=0; i<indicegasto; i++) {
+    		if(gastotipo[i].equals(tipo)){
+    			sum = sum + gastod[i];
+    		}
+    	}
+    	System.out.println("Total de gastos en " + tipo + " " + sum + "€");
+    }
     
+}
 }
